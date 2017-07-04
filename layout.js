3b750b36ce1109b37ffe9632e5e5f1319c039478
Michael Farrelly <farrellyt@Marikos-MacBook-Pro.local>
2017-07-03 20:16:53 -0400

    weather div work

---
+++ layout.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+
+// var currentTime = null,
+// 	date = null;
+
+// function update() {
+//     date = moment(new Date())
+//     currentTime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+// };
+
+//     currentTime = $('#currentTime');
+//     update();
+//     setInterval(update, 1000);
+//     $('#runningClock').html(currentTime);
+// });

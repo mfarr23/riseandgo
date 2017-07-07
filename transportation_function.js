fdb81b86cae35af7a98ad1bf06514e6ca15be4dd
Akandu <ajnwosu@gmail.com>
2017-07-07 19:47:33 -0400

    Transporatation

--- transportation_function.js
+++ transportation_function.js
@@ -17,6 +17,7 @@ var estimatedArrivalTimeUser = "";
 var estimatedArrivalTimeUserHour = 0;
 var estimatedArrivalTimeUserMinutes = 0;
 var estimatedArrivalTimeGoogle = ""; // 8:00 AM from google API
+
 var timeYouWantToArrive = "8:00"; // 8:00 AM
 // var alarm  = "05/07/2017 08:00";
 
@@ -87,8 +88,110 @@ function alertPossibleLateness() {
 		alert("You are late! Leave NOW!!")
 	}
 };
+ 
+// google.initMap.
+
+// Goole Distance Matrix API--------------*************************************
 
 
+function initMap() {
+        var bounds = new google.maps.LatLngBounds;
+        var markersArray = [];
+
+        var origin1 = "Philadelphia";
+        var origin2 = '';
+        var destinationA = "New York";
+        var destinationB = {lat: 50.087, lng: 14.421};
+
+        var destinationIcon = 'https://chart.googleapis.com/chart?' +
+            'chst=d_map_pin_letter&chld=D|FF0000|000000';
+        var originIcon = 'https://chart.googleapis.com/chart?' +
+            'chst=d_map_pin_letter&chld=O|FFFF00|000000';
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 55.53, lng: 9.4},
+          zoom: 10
+        });
+        var geocoder = new google.maps.Geocoder;
+
+        var service = new google.maps.DistanceMatrixService;
+        service.getDistanceMatrix({
+          origins: [origin1],
+          destinations: [destinationA],
+          travelMode: 'DRIVING',
+          unitSystem: google.maps.UnitSystem.IMPERIAL,
+          avoidHighways: false,
+
+          avoidTolls: false
+        }, 
+
+        function(response, status) {
+          console.log(response)
+          if (status !== 'OK') {
+            alert('Error was: ' + status);
+          } else {
+            var originList = response.originAddresses;
+            var destinationList = response.destinationAddresses;
+            var outputDiv = document.getElementById('output');
+            outputDiv.innerHTML = '';
+            deleteMarkers(markersArray);
+
+            var showGeocodedAddressOnMap = function(asDestination) {
+              var icon = asDestination ? destinationIcon : originIcon;
+              return function(results, status) {
+                if (status === 'OK') {
+                  map.fitBounds(bounds.extend(results[0].geometry.location));
+                  markersArray.push(new google.maps.Marker({
+                    map: map,
+                    position: results[0].geometry.location,
+                    icon: icon
+                  }));
+                } else {
+                  alert('Geocode was not successful due to: ' + status);
+                }
+              };
+            };
+
+            for (var i = 0; i < originList.length; i++) {
+              var results = response.rows[i].elements;
+              
+              geocoder.geocode({'address': originList[i]},
+                  showGeocodedAddressOnMap(false));
+              for (var j = 0; j < results.length; j++) {
+                geocoder.geocode({'address': destinationList[j]},
+                    showGeocodedAddressOnMap(true));
+                outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
+                    ': ' + results[j].distance.text + ' in ' +
+                    results[j].duration.text + '<br>';
+
+                 var element = results[j];
+
+                 // distance variable
+                 var distance = element.distance.text;
+                 console.log(distance)
+
+                 // Duration variable
+                 var duration = element.duration.value;
+                 console.log(duration / 60 + "mins")
+        
+              }
+            }
+          }
+
+
+        });
+      }
+
+      function deleteMarkers(markersArray) {
+        for (var i = 0; i < markersArray.length; i++) {
+          markersArray[i].setMap(null);
+        }
+        markersArray = [];
+      }
+
+  
+
+       console.log(initMap);
+// console.log(theDuration)
 // SCRAPWORK------------------------******************************
 
 // // --------difference inbetween current time and alarm time

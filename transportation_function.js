d6bee185797e77e85558e8cdcd4279d03ce2df05
Michael Farrelly <farrellyt@Marikos-MacBook-Pro.local>
2017-07-07 20:24:18 -0400

    test

--- transportation_function.js
+++ transportation_function.js
@@ -1,130 +1,128 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-var currentTimeHours = null,
-	date = null;
-var currentTimeMinutes = null;
-var currentTime = null;
-var commuteTime = 98;
-var timeToGetReady = 23;
-var totalTimeNeededBeforeDepartureHours = 0;
-var	totalTimeNeededBeforeDepartureMinutes = 0;
-// Gives you the ETA by adding the total commute time and the time to get ready with the current time
-// ie. 50 mins to get ready plus current Time: 8:00 = 8:50
-var estimatedArrivalTimeUser = "";
-var estimatedArrivalTimeUserHour = 0;
-var estimatedArrivalTimeUserMinutes = 0;
-var estimatedArrivalTimeGoogle = ""; // 8:00 AM from google API
-<<<<<<< HEAD
+// var currentTimeHours = null,
+// 	date = null;
+// var currentTimeMinutes = null;
+// var currentTime = null;
+// var commuteTime = 98;
+// var timeToGetReady = 23;
+// var totalTimeNeededBeforeDepartureHours = 0;
+// var	totalTimeNeededBeforeDepartureMinutes = 0;
+// // Gives you the ETA by adding the total commute time and the time to get ready with the current time
+// // ie. 50 mins to get ready plus current Time: 8:00 = 8:50
+// var estimatedArrivalTimeUser = "";
+// var estimatedArrivalTimeUserHour = 0;
+// var estimatedArrivalTimeUserMinutes = 0;
+// var estimatedArrivalTimeGoogle = ""; // 8:00 AM from google API
 
-var timeYouWantToArrive = "8:00"; // 8:00 AM
-// var alarm  = "05/07/2017 08:00";
-
-=======
-var timeYouWantToArrive = "8:00"; 
->>>>>>> 4880e0d5fcb55ef9d66ba34b43dd773b8f850b00
-
-$('#timeYouWantToArrive').html(timeYouWantToArrive);
-
-// BREAKING DOWN TIME INTO HOURS AND MINS
-	var timeToGetReadyHours = Math.floor(timeToGetReady / 60);          
-	var timeToGetReadyMinutes = timeToGetReady % 60;
-
-	  $('#timeToGetReady').html(timeToGetReadyHours + " hours, " + timeToGetReadyMinutes + " minutes");
-
-	var commuteTimeHours = Math.floor(commuteTime / 60);          
-	var commuteTimeMinutes = commuteTime % 60;
-
-	  $('#commuteTime').html(commuteTimeHours + " hours, " + commuteTimeMinutes + " minutes");
-
-	var totalTimeNeededBeforeDepartureHours = Math.floor((commuteTime + timeToGetReady) / 60);
-	var	totalTimeNeededBeforeDepartureMinutes = (commuteTime + timeToGetReady) % 60;
-
-// SOMETIMES YOU GET 21:6 for six minutes so I put in this if else statement
-if (totalTimeNeededBeforeDepartureMinutes < 10) {
-	$('#totalTimeNeededBeforeDeparture').html(totalTimeNeededBeforeDepartureHours + " hours, 0" + totalTimeNeededBeforeDepartureMinutes + " minutes");
-}
-else {
-	$('#totalTimeNeededBeforeDeparture').html(totalTimeNeededBeforeDepartureHours + " hours, " + totalTimeNeededBeforeDepartureMinutes + " minutes");
-}
+// var timeYouWantToArrive = "8:00"; // 8:00 AM
+// // var alarm  = "05/07/2017 08:00";
 
 
-// -----------------CONTINUOUS CLOCK UPDATE
+// var timeYouWantToArrive = "8:00"; 
 
-function update() {
-    date = moment(new Date())
-	currentTime.html(date.format('HH:mm:ss'));
-	currentTimeHours.html(date.format('HH'));
-    currentTimeMinutes.html(date.format('mm'));
-    // currentTimeDate.html(date.format("DD/MM/YYYY HH:mm"));
+// $('#timeYouWantToArrive').html(timeYouWantToArrive);
 
-// converts current time and total time needed before departure into only minutes 1:10 = 70 mins
-	var currentTimeConvertedMinutes = ((parseInt(currentTimeHours[0].innerHTML)) * 60) + (parseInt(currentTimeMinutes[0].innerHTML));
-    var totalTimeNeededBeforeDepartureConvertedMinutes = (commuteTimeHours * 60) + commuteTimeMinutes + (timeToGetReadyHours * 60) + timeToGetReadyMinutes;
+// // BREAKING DOWN TIME INTO HOURS AND MINS
+// 	var timeToGetReadyHours = Math.floor(timeToGetReady / 60);          
+// 	var timeToGetReadyMinutes = timeToGetReady % 60;
 
-// finds hour
-	var estimatedArrivalTimeUserHour = Math.floor((parseInt(currentTimeConvertedMinutes) + totalTimeNeededBeforeDepartureConvertedMinutes) / 60);
-	console.log(estimatedArrivalTimeUserHour);
+// 	  $('#timeToGetReady').html(timeToGetReadyHours + " hours, " + timeToGetReadyMinutes + " minutes");
 
-// finds minutes
-	var estimatedArrivalTimeUserMinutes = (currentTimeConvertedMinutes + totalTimeNeededBeforeDepartureConvertedMinutes) % 60;
-	console.log(estimatedArrivalTimeUserMinutes);
+// 	var commuteTimeHours = Math.floor(commuteTime / 60);          
+// 	var commuteTimeMinutes = commuteTime % 60;
 
-// combines minutes and hours
-	var estimatedArrivalTimeUser = (estimatedArrivalTimeUserHour + ":" + estimatedArrivalTimeUserMinutes);
-    $('#estimatedArrivalTimeUser').html(estimatedArrivalTimeUser);
-};
+// 	  $('#commuteTime').html(commuteTimeHours + " hours, " + commuteTimeMinutes + " minutes");
 
-// sets times equal to divs and updates every second
-    currentTime = $('#currentTime')
-    currentTimeHours = $('#currentTimeHours')
-    currentTimeMinutes = $('#currentTimeMinutes')
-    update();
-    setInterval(update, 1000);
+// 	var totalTimeNeededBeforeDepartureHours = Math.floor((commuteTime + timeToGetReady) / 60);
+// 	var	totalTimeNeededBeforeDepartureMinutes = (commuteTime + timeToGetReady) % 60;
+
+// // SOMETIMES YOU GET 21:6 for six minutes so I put in this if else statement
+// if (totalTimeNeededBeforeDepartureMinutes < 10) {
+// 	$('#totalTimeNeededBeforeDeparture').html(totalTimeNeededBeforeDepartureHours + " hours, 0" + totalTimeNeededBeforeDepartureMinutes + " minutes");
+// }
+// else {
+// 	$('#totalTimeNeededBeforeDeparture').html(totalTimeNeededBeforeDepartureHours + " hours, " + totalTimeNeededBeforeDepartureMinutes + " minutes");
+// }
 
 
-});
-// END DOCUMENT.READY
+// // -----------------CONTINUOUS CLOCK UPDATE
 
-// FOR SOME REASON THE YOUTUBE ONLY LOADS WHEN OUTSIDE OF DOCUMENT.READY below:
+// function update() {
+//     date = moment(new Date())
+// 	currentTime.html(date.format('HH:mm:ss'));
+// 	currentTimeHours.html(date.format('HH'));
+//     currentTimeMinutes.html(date.format('mm'));
+//     // currentTimeDate.html(date.format("DD/MM/YYYY HH:mm"));
 
-// This code loads the IFrame Player API code asynchronously.
-	var tag = document.createElement('script');
-	tag.src = "http://www.youtube.com/player_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// // converts current time and total time needed before departure into only minutes 1:10 = 70 mins
+// 	var currentTimeConvertedMinutes = ((parseInt(currentTimeHours[0].innerHTML)) * 60) + (parseInt(currentTimeMinutes[0].innerHTML));
+//     var totalTimeNeededBeforeDepartureConvertedMinutes = (commuteTimeHours * 60) + commuteTimeMinutes + (timeToGetReadyHours * 60) + timeToGetReadyMinutes;
 
-// 3. This function creates an <iframe> (and YouTube player) after the API code downloads.
-	var player;
-	var date = new Date();
+// // finds hour
+// 	var estimatedArrivalTimeUserHour = Math.floor((parseInt(currentTimeConvertedMinutes) + totalTimeNeededBeforeDepartureConvertedMinutes) / 60);
+// 	console.log(estimatedArrivalTimeUserHour);
 
-// input wake up time here ie. 8:30 am (date.getHours() === 8 && date.getMinutes() === 30)
-// if its the time you want to wake up, play youtube video, otherwise load it without autoplay
-// NEED TO SET AN INTERVAL TO RELOAD THIS FUNCTION EVERY MINUTE
-	function onYouTubePlayerAPIReady() {
-		    if(date.getHours() === 15 && date.getMinutes() === 22) {
-	        player = new YT.Player('player', {
-	          playerVars: { 'autoplay': 1, 'controls': 1,'autohide':1,'wmode':'opaque' },
-	          videoId: 'uzt6vlpdZWM',
-	          events: {
-	            'onReady': onPlayerReady}
-	        })
-	    }
-	    else {
-	        player = new YT.Player('player', {
-	    	playerVars: { 'autoplay': 0, 'controls': 1,'autohide':1,'wmode':'opaque' },
-	          videoId: 'uzt6vlpdZWM',
-	          events: {
-	            'onReady': onPlayerReady}
-	    	})
-	    	// console.log("Its not time to wake up");
-	    	onYouTubePlayerAPIReady();
+// // finds minutes
+// 	var estimatedArrivalTimeUserMinutes = (currentTimeConvertedMinutes + totalTimeNeededBeforeDepartureConvertedMinutes) % 60;
+// 	console.log(estimatedArrivalTimeUserMinutes);
+
+// // combines minutes and hours
+// 	var estimatedArrivalTimeUser = (estimatedArrivalTimeUserHour + ":" + estimatedArrivalTimeUserMinutes);
+//     $('#estimatedArrivalTimeUser').html(estimatedArrivalTimeUser);
+// };
+
+// // sets times equal to divs and updates every second
+//     currentTime = $('#currentTime')
+//     currentTimeHours = $('#currentTimeHours')
+//     currentTimeMinutes = $('#currentTimeMinutes')
+//     update();
+//     setInterval(update, 1000);
+
+
+// });
+// // END DOCUMENT.READY
+
+// // FOR SOME REASON THE YOUTUBE ONLY LOADS WHEN OUTSIDE OF DOCUMENT.READY below:
+
+// // This code loads the IFrame Player API code asynchronously.
+// 	var tag = document.createElement('script');
+// 	tag.src = "http://www.youtube.com/player_api";
+// 	var firstScriptTag = document.getElementsByTagName('script')[0];
+// 	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// // 3. This function creates an <iframe> (and YouTube player) after the API code downloads.
+// 	var player;
+// 	var date = new Date();
+
+// // input wake up time here ie. 8:30 am (date.getHours() === 8 && date.getMinutes() === 30)
+// // if its the time you want to wake up, play youtube video, otherwise load it without autoplay
+// // NEED TO SET AN INTERVAL TO RELOAD THIS FUNCTION EVERY MINUTE
+// 	function onYouTubePlayerAPIReady() {
+// 		    if(date.getHours() === 15 && date.getMinutes() === 22) {
+// 	        player = new YT.Player('player', {
+// 	          playerVars: { 'autoplay': 1, 'controls': 1,'autohide':1,'wmode':'opaque' },
+// 	          videoId: 'uzt6vlpdZWM',
+// 	          events: {
+// 	            'onReady': onPlayerReady}
+// 	        })
+// 	    }
+// 	    else {
+// 	        player = new YT.Player('player', {
+// 	    	playerVars: { 'autoplay': 0, 'controls': 1,'autohide':1,'wmode':'opaque' },
+// 	          videoId: 'uzt6vlpdZWM',
+// 	          events: {
+// 	            'onReady': onPlayerReady}
+// 	    	})
+// 	    	// console.log("Its not time to wake up");
+// 	    	onYouTubePlayerAPIReady();
 	    	
-	    }
-// The API will call this function when the video player is ready.
-	function onPlayerReady(event) {
-		event.target.mute();
-		}
-};
+// 	    }
+// // The API will call this function when the video player is ready.
+// 	function onPlayerReady(event) {
+// 		event.target.mute();
+// 		}
+// };
 
 
 // --------Scrap
@@ -142,15 +140,14 @@ function update() {
 // NEED A IF THEN STATEMENT
 // IF TOTAL COMMUTE TIME IS LESS THAN 30 MINS THEN SOUND ALARM AT 7AM
 // IF MORE THAN 30 MINS THEN SOUND ALARM AT 7AM MINUS EXTRA TIME NEEDED
-<<<<<<< HEAD
-function alertPossibleLateness() {
-	if ((estimatedArrivalTimeGoogle) === estimatedArrivalTimeUser) {
-		alert("You must leave now")
-	}
-	else if ((estimatedArrivalTimeGoogle) > estimatedArrivalTimeUser) {
-		alert("You are late! Leave NOW!!")
-	}
-};
+// function alertPossibleLateness() {
+// 	if ((estimatedArrivalTimeGoogle) === estimatedArrivalTimeUser) {
+// 		alert("You must leave now")
+// 	}
+// 	else if ((estimatedArrivalTimeGoogle) > estimatedArrivalTimeUser) {
+// 		alert("You are late! Leave NOW!!")
+// 	}
+// };
  
 // google.initMap.
 
@@ -162,7 +159,7 @@ function initMap() {
         var markersArray = [];
 
         var origin1 = "Philadelphia";
-        var origin2 = '';
+        var origin2 = "";
         var destinationA = "New York";
         var destinationB = {lat: 50.087, lng: 14.421};
 
@@ -188,7 +185,7 @@ function initMap() {
         }, 
 
         function(response, status) {
-          console.log(response)
+          console.log(initMap.response)
           if (status !== 'OK') {
             alert('Error was: ' + status);
           } else {
@@ -233,9 +230,9 @@ function initMap() {
                  console.log(distance)
 
                  // Duration variable
-                 var duration = element.duration.value;
+                 var theDuration = element.duration.value;
                  console.log(duration / 60 + "mins")
-        
+
               }
             }
           }
@@ -249,13 +246,9 @@ function initMap() {
           markersArray[i].setMap(null);
         }
         markersArray = [];
-      }
+      };
 
   
-
-       console.log(initMap);
-// console.log(theDuration)
-=======
 // function alertPossibleLateness() {
 //       if (parseInt(estimatedArrivalTimeUser) === parseInt("9:19")) {
 //       	onYouTubePlayerAPIReady();
@@ -268,7 +261,6 @@ function initMap() {
 
 
 
->>>>>>> 4880e0d5fcb55ef9d66ba34b43dd773b8f850b00
 // SCRAPWORK------------------------******************************
 
 // // --------difference inbetween current time and alarm time
